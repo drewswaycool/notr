eb606683f4ef38604188bb205d7b9da6df93598c
Drew McMurry <andrewmcmurry@gmail.com>
2019-10-31 11:04:01 -0600

    adds initial formik use

--- src/components/EmptyNote/EmptyNote.jsx
+++ src/components/EmptyNote/EmptyNote.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Formik } from 'formik';
 
 /** @jsx jsx */
 import { css, jsx } from '@emotion/core'
@@ -11,10 +12,20 @@ const emptyNoteStyles = css`
   width: 900px;
 `
 
-const textInput = css`
+const titleInput = css`
   display:block;
   font-size: 40px;
   height: 50px;
+  width: 500px;
+  margin-top:5px;
+  background: none;
+  border: none;
+`
+
+const textInput = css`
+  display:block;
+  font-size: 20px;
+  height: 30px;
   width: 500px;
   margin-top:5px;
   background: none;
@@ -36,11 +47,66 @@ export default function EmptyNote(props) {
 
   return (
     <div css={emptyNoteStyles}>
-      <form>
-        <input css={textInput} placeholder="Title" type="text" className="titleInput" />
-        <input css={textInput} placeholder="Description" type="text" className="descriptionInput" />
-        <textarea css={textareaStyles}></textarea>
-      </form>
+      <Formik
+        initialValues={{ title: '', description: '', content: '' }}
+        validate={values => {
+          let errors = {};
+          if (!values.title) {
+            errors.title = 'Required';
+          }
+          return errors;
+        }}
+        onSubmit={(values, { setSubmitting }) => {
+          setTimeout(() => {
+            alert(JSON.stringify(values, null, 2));
+            setSubmitting(false);
+          }, 400);
+        }}
+      >
+        {({
+          values,
+          errors,
+          touched,
+          handleChange,
+          handleBlur,
+          handleSubmit,
+          isSubmitting,
+          /* and other goodies */
+        }) => (
+            <form onSubmit={handleSubmit}>
+              <input
+                placeholder="Title"
+                css={titleInput}
+                type="text"
+                name="title"
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.title}
+              />
+              {errors.title && touched.title && errors.title}
+              <input
+                placeholder="Description"
+                css={textInput}
+                type="text"
+                name="description"
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.description}
+              />
+              {errors.description && touched.description && errors.description}
+              <textarea
+                css={textareaStyles}
+                name="content"
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.content}
+              />
+              <button type="submit" disabled={isSubmitting}>
+                Submit
+          </button>
+            </form>
+          )}
+      </Formik>
     </div>
   )
 
